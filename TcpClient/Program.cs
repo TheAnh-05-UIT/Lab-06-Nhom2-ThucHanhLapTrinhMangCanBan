61778022f3c779cffc12e293217d1b0914b3d0e9
TheAnh-05 <nguyenquangtheanh2005@gmail.com>
2025-12-18 13:59:25 +0700

    update

--- TcpClient/Program.cs
+++ TcpClient/Program.cs
@@ -16,7 +16,7 @@ namespace TcpClient
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frm_KhachHang());
         }
     }
 }
