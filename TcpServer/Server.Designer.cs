d88d12ec4906a68a7aa89f9a365af792d55441ec
iamabighotdog <anhhao006@gmail.com>
2025-12-18 14:26:32 +0700

    commit 1

---
+++ TcpServer/Server.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace TcpServer
+{
+    partial class Server
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.historyTextBox = new System.Windows.Forms.RichTextBox();
+            this.listenButton = new System.Windows.Forms.Button();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // historyTextBox
+            // 
+            this.historyTextBox.Location = new System.Drawing.Point(85, 62);
+            this.historyTextBox.Name = "historyTextBox";
+            this.historyTextBox.Size = new System.Drawing.Size(627, 344);
+            this.historyTextBox.TabIndex = 0;
+            this.historyTextBox.Text = "";
+            // 
+            // listenButton
+            // 
+            this.listenButton.Location = new System.Drawing.Point(608, 22);
+            this.listenButton.Name = "listenButton";
+            this.listenButton.Size = new System.Drawing.Size(104, 34);
+            this.listenButton.TabIndex = 1;
+            this.listenButton.Text = "Listen";
+            this.listenButton.UseVisualStyleBackColor = true;
+            this.listenButton.Click += new System.EventHandler(this.listenButton_Click);
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.statusLabel.Location = new System.Drawing.Point(81, 28);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(67, 20);
+            this.statusLabel.TabIndex = 2;
+            this.statusLabel.Text = "Status: ";
+            // 
+            // Server
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.statusLabel);
+            this.Controls.Add(this.listenButton);
+            this.Controls.Add(this.historyTextBox);
+            this.Name = "Server";
+            this.Text = "Server";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox historyTextBox;
+        private System.Windows.Forms.Button listenButton;
+        private System.Windows.Forms.Label statusLabel;
+    }
+}
---
+++ TcpServer/Server.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace TcpServer
+{
+    public partial class Server : Form
+    {
+        public Server()
+        {
+            InitializeComponent();
+        }
+        private Thread listenThread;
+        private TcpListener tcpListener;
+        private bool stopServer = true;
+        private readonly int _serverPort = 8080;
+        private delegate void SafeCallDelegate(string text);
+        public void UpdateHistorySafeCall(string text)
+        {
+            if (stopServer)
+            {
+                return;
+            }
+            if (historyTextBox.InvokeRequired)
+            {
+                var d = new SafeCallDelegate(UpdateHistorySafeCall);
+                historyTextBox.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                historyTextBox.AppendText(text + Environment.NewLine);
+            }
+        }
+
+        public string ReadMenuFromFile()
+        {
+            string menu = "";
+            try
+            {
+                using (StreamReader sr = new StreamReader("menu.txt"))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        menu += line + Environment.NewLine;
+                    }
+
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Lỗi đọc file: " + e.Message);
+            }
+            return menu;
+        }
+        private void Server_Load(object sender, EventArgs e)
+        {
+            string menu = ReadMenuFromFile();
+            UpdateHistorySafeCall("Menu đã load");
+            UpdateHistorySafeCall(menu);
+        }
+        public async Task Listen()
+        {
+            try
+            {
+                tcpListener = new TcpListener(IPAddress.Any, _serverPort);
+                tcpListener.Start();
+                statusLabel.Text = @"Status: Server is listening on port " + _serverPort;
+                Server_Load(this, null);
+                while (!stopServer)
+                {
+                    //TcpClient tcpClient = await tcpListener.AcceptTcpClientAsync();
+                    //var ns = tcpClient.GetStream();
+                    ////var sReader = new StreamReader(ns, Encoding.UTF8);
+                    //// var sWriter = new StreamWriter(ns, Encoding.UTF8) { AutoFlush = true };
+                    //byte[] data = Encoding.UTF8.GetBytes();
+
+                    //string clientEndPoint = tcpClient.Client.RemoteEndPoint.ToString();
+                    //UpdateHistorySafeCall("Connected to client: " + clientEndPoint);
+                    //string menu = ReadMenuFromFile();
+                    //await sWriter.WriteAsync("" +menu);
+                    //NetworkStream stream = new NetworkStream();
+                    //byte[] buffer = new byte[1024];
+                    //TcpClient tcpClient = await tcpListener.AcceptTcpClientAsync();
+                    //stream = tcpClient.GetStream();
+                    //string clientEndPoint = tcpClient.Client.RemoteEndPoint.ToString();
+                    //UpdateHistorySafeCall("Connected to client: " + clientEndPoint);
+                    //int bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length);
+                    //string menu = "Customer" + ReadMenuFromFile();
+                    //UpdateHistorySafeCall(menu);
+                    //byte[] menuBytes = Encoding.UTF8.GetBytes(menu);
+                    //await stream.WriteAsync(menuBytes, 0, menuBytes.Length);
+                    //stream.Close();
+                    //tcpClient.Close();
+                    TcpClient tcpClient = await tcpListener.AcceptTcpClientAsync();
+                    _ = HandlerGetClient(tcpClient);
+                }
+            }
+            catch (SocketException sockEx)
+            {
+                MessageBox.Show(sockEx.Message);
+            }
+        }
+        private async Task HandlerGetClient(TcpClient tcpClient)
+        {
+            using (tcpClient)
+            {
+                NetworkStream stream = new NetworkStream(tcpClient.Client);
+                string clientEndPoint = tcpClient.Client.RemoteEndPoint.ToString();
+                UpdateHistorySafeCall("Connected to client: " + clientEndPoint);
+                string menu = "Customer" + ReadMenuFromFile();
+                UpdateHistorySafeCall(menu);
+                byte[] menuBytes = Encoding.UTF8.GetBytes(menu);
+                await stream.WriteAsync(menuBytes, 0, menuBytes.Length);
+                stream.Close();
+            }
+        }
+        private void listenButton_Click(object sender, EventArgs e)
+        {
+            if (stopServer)
+            {
+                stopServer = false;
+                listenButton.Text = "Stop";
+                listenThread = new Thread(async () => await Listen());
+                listenThread.Start();
+            }
+            else
+            {
+                stopServer = true;
+                listenButton.Text = "Listen";
+                tcpListener.Stop();
+                listenThread.Abort();
+                statusLabel.Text = @"Status: Server is stopped";
+            }
+        }
+    }
+}
